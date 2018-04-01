f65d047bbcfc01bbf7ec2d7b3b2f0060a6410df6
grrolland <gregoire.rolland@s2e-net.com>
2018-04-01 16:59:35 +0200

    Add test case

--- src/main/java/com/flutech/hcshm/Configuration.java
+++ src/main/java/com/flutech/hcshm/Configuration.java
@@ -32,7 +32,7 @@ public class Configuration {
     /**
      * Default Bind Address
      */
-    public static final String DEFAULT_BIND_ADDRESS = "localhost";
+    public static final String DEFAULT_BIND_ADDRESS = "127.0.0.1";
 
     /**
      * Private default constructor
--- src/test/java/com/flutech/hcshm/HcshmTestCase.java
+++ src/test/java/com/flutech/hcshm/HcshmTestCase.java
@@ -60,7 +60,7 @@ public class HcshmTestCase {
     @Before
     public void before() {
         try {
-            Socket sock = new Socket(InetAddress.getByName("localhost"), 4321);
+            Socket sock = new Socket(InetAddress.getByName("127.0.0.1"), 4321);
             reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
             writer = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
         }
