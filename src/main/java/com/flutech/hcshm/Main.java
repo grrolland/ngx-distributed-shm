c549ccebf54bae871ce3c431e87aeda7204dee5e
grrolland <gregoire.rolland@s2e-net.com>
2018-04-05 09:51:50 +0200

    Sonar Fixes

--- src/main/java/com/flutech/hcshm/Main.java
+++ src/main/java/com/flutech/hcshm/Main.java
@@ -17,7 +17,6 @@
  */
 package com.flutech.hcshm;
 
-import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import io.vertx.core.DeploymentOptions;
