9b45165780f61f418547bfecdcd6b77e4a8fd957
Cristian Chiru <cristian.chiru@revomatico.com>
2020-07-08 03:27:08 +0300

    Update hazelcast to 4.0.2 and add hazelcast-kubernetes

--- src/main/java/io/github/grrolland/hcshm/ShmRegionLocator.java
+++ src/main/java/io/github/grrolland/hcshm/ShmRegionLocator.java
@@ -18,7 +18,7 @@
 package io.github.grrolland.hcshm;
 
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IMap;
+import com.hazelcast.map.IMap;
 
 import java.io.Serializable;
 
