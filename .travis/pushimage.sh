0a8a10c2b85b4a1f89f68b0955595f8a96c486f7
Grégoire Rolland <gregoire.rolland@cbp-group.com>
2021-10-12 12:33:26 +0200

    Bump dependency versions + publish docker image on Quay.io

--- .travis/pushimage.sh
+++ .travis/pushimage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 export TAG=$(if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi)
-export IMAGE_NAME=quay.io/grrolland/ngx-distibuted-shm
+export IMAGE_NAME=quay.io/grrolland/ngx-distributed-shm
 docker build -f Dockerfile -t $IMAGE_NAME:$TAG .
 docker push $IMAGE_NAME
